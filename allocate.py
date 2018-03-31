7ea970eecc0742082b9aeb5161a19c25cad11c5d
rkteddy <838474413@qq.com>
2018-04-01 00:59:53 +0800

    Add first fit assign scheme

--- allocate.py
+++ allocate.py
@@ -1,3 +1,7 @@
+import copy
+import random
+import math
+
 # physical server class definition
 class physical_server:
 
@@ -26,3 +30,32 @@ class virtual_machine:
         self.cpu = cpu
         self.mem = mem
         self.cnt = 0
+
+
+def isable_locate(server, machine):
+    if machine.cpu > server.rest_cpu or machine.mem > server.rest_mem:
+        return False
+    else:
+        return True
+
+
+def assign_flavors(sample_server, machine_list):
+    server_list = [copy.deepcopy(sample_server)]
+    for i in range(len(machine_list)):
+        j = 0
+        while j < len(server_list) and not isable_locate(server_list[j], machine_list[i]):
+            j += 1
+        if j < len(server_list) and isable_locate(server_list[j], machine_list[i]):
+            server_list[j].add_vm(machine_list[i])
+        else:
+            server_list.append(copy.deepcopy(sample_server))
+            server_list[-1].add_vm(machine_list[i])
+
+    # for i in range(len(server_list)):
+    #     print("Server " + str(i+1) + ": ")
+    #     for j in range(len(server_list[i].vm_cnt)):
+    #         print(server_list[i].vm_cnt[j])
+
+    return server_list
+
+
