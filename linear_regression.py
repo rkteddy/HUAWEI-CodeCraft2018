8b3989d5b7b23ff666ab7ca9a6d1048550777efd
rkteddy <838474413@qq.com>
2018-04-11 02:27:17 +0800

    Add ridge regression

--- linear_regression.py
+++ linear_regression.py
@@ -9,5 +9,9 @@ class linear_regression:
     def lse_fit(self, X, y):
         self.parm = (X.transpose() * X).inverse() * X.transpose() * y
 
+    def ridge_fit(self, X, y, lambda_):
+        lambda_I = matrix.identity(X.cols_)
+        self.parm = (X.transpose() * X + lambda_I).inverse() * X.transpose() * y
+
     def predict(self, X):
         return (X * self.parm)[0, 0]
